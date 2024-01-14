323203ed719134b41734bc87a71ad039777eabca
ayaosama290 <orakel999@gmail.com>
2024-01-13 16:24:54 -0800

    j

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for i in range(10):
         if i == j:
             continue
         elif i+j != 17:
-            print(f"{i}{j}, ", end="")
+            print("{}{}, ".format(i,j), end="")
         else:
-            print(f"{i}{j}")
+            print("{}{}".format(i,j))
