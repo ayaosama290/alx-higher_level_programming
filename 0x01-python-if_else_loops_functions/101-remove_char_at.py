c4648128346a2b5ac9fb970ba46a0e963c38eea0
ayaosama290 <orakel999@gmail.com>
2024-01-15 17:54:35 -0800

    k

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    holder1 = ""
+    holder2 = ""
+    if n > len(str):
+        return str
+    if n < 0:
+        n = len(str) + n
+    for i in range(len(str)):
+        if i == n:
+            holder1 = str[:n]
+            holder2 = str[n+1:]
+            break
+    str = holder1 + holder2
+    return str
