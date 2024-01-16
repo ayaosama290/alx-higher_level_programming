9cd839bb84468c4e98b7c2ac1f9a2e4f3d733f55
ayaosama290 <orakel999@gmail.com>
2024-01-16 06:01:03 -0800

    j

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from add_0 import add
+def print_add():
+    a = 1
+    b = 2
+    addvar = add(a,b)
+    print("{} + {} = {}".format(a, b, addvar))
+if __name__ == "__main__":
+    print_add()
